3d82f878c4d19af82952b899ba56f6884a397cc6
Ken Aho <kenaho1@gmail.com>
2011-05-04 07:13:40 +0000

    version 0.3-32

--- R/lmu.tck.r
+++ R/lmu.tck.r
@@ -3,9 +3,15 @@ par(mar=c(0,0,0,0))
 layout(matrix(c(rep(1,1),rep(2,2),rep(3,1),rep(4,1),rep(5,1)),2,3, byrow = TRUE))
 plot(seq(1,10),seq(1,10),type="n",xlab="",ylab="",xaxt="n",yaxt="n",bty="n")
 text(2.5,5,"Y = ",font=2,cex=2)
+segments(c(4.5,7),c(2,2),c(4.5,7),c(9,9),lwd=2)
+segments(c(4.5,7),c(2,9),c(4.7,6.8),c(2,9),lwd=2)
+segments(c(4.5,7),c(9,2),c(4.7,6.8),c(9,2),lwd=2)
 legend("center",ncol=1,legend=Y,bty="n",cex=.9)
 plot(seq(1,10),seq(1,10),type="n",xlab="",ylab="",xaxt="n",yaxt="n",bty="n")
 text(4,5, "X = ", font=2,cex=2)
+segments(c(4.5,6.6),c(2,2),c(4.5,6.6),c(9,9),lwd=2)
+segments(c(4.5,6.6),c(2,9),c(4.6,6.5),c(2,9),lwd=2)
+segments(c(4.5,6.6),c(9,2),c(4.6,6.5),c(9,2),lwd=2)
 legend("center",ncol=3,legend=c(X1,X2,X1X2),bty="n",cex=.9)
 #SS
 
@@ -19,29 +25,38 @@ legend("center",ncol=3,legend=c(X1,X2,X1X2),bty="n",cex=.9)
  SSE.X1iX2<-anova(lm(Y~X1+X2+X1X2))$"Sum Sq"[4]
  if(change.order==FALSE)anovaI<-anova(lm(Y~X1+X2+X1X2))
  if(change.order==TRUE)anovaI<-anova(lm(Y~X2+X1+X1X2))
- 
+ require(car)
+ if(change.order==FALSE)anovaII<-Anova(lm(Y~X1+X2+X1X2))
+ if(change.order==TRUE)anovaII<-Anova(lm(Y~X2+X1+X1X2))
+ if(change.order==FALSE)anovaIII<-Anova(lm(Y~X1+X2+X1X2),type="III")
+ if(change.order==TRUE)anovaIII<-Anova(lm(Y~X2+X1+X1X2),type="III")
  ##type I
  
  plot(seq(1,10),seq(1,10),type="n",xlab="",ylab="",xaxt="n",yaxt="n",bty="n")
- 
+ rect(1,1,10,10,col="wheat")
  legend("top",inset=.2,title=ifelse(change.order==FALSE,"Type I Sum of Squares\n\nModel: Y = X1 + X2 + X1:X2","Type I Sum of Squares\n\nModel: Y = X2 + X1 + X1:X2"),legend=c(
  ifelse(change.order==FALSE,paste("SSE.X1 = ",round(SSE.mu-SSE.X1,3)),paste("SSE.X2 = ",round(SSE.mu-SSE.X2,3))), ifelse(change.order==FALSE,paste("SSE.X2 = ",round(SSE.X1-SSE.X1pX2,3)),paste("SSE.X1 = ",round(SSE.X2-SSE.X1pX2,3))),paste("SSE.X1:X2 = ",round(SSE.X1pX2-SSE.X1iX2,3)),paste("SSE = ",round(SSE.X1iX2,3))),bty="n")
 
-legend("bottom",title="P-value",legend=c(paste("X1 = ",round(anovaI$"Pr(>F)"[1],6)),paste("X2 = ",round(anovaI$"Pr(>F)"[2],6)),paste("X1X2 = ",round(anovaI$"Pr(>F)"[3],6))),bty="n",inset=.2) 
+legend("bottom",title="P-value",legend=c(paste("X1 = ",round(anovaI$"Pr(>F)"[1],6)),paste("X2 = ",round(anovaI$"Pr(>F)"[2],6)),paste("X1:X2 = ",round(anovaI$"Pr(>F)"[3],6))),bty="n",inset=.2) 
  
 #type II 
  
 plot(seq(1,10),seq(1,10),type="n",xlab="",ylab="",xaxt="n",yaxt="n",bty="n")
- 
+rect(1,1,10,10,col="wheat3") 
  legend("top",inset=.2,title=ifelse(change.order==FALSE,"Type II Sum of Squares\n\nModel: Y = X1 + X2 + X1:X2","Type II Sum of Squares\n\nModel: Y = X2 + X1 + X1:X2"),legend=c(
  ifelse(change.order==FALSE,paste("SSE.X1 = ",round(SSE.X2-SSE.X1pX2,3)),paste("SSE.X2 = ",round(SSE.X1-SSE.X1pX2,3))), ifelse(change.order==FALSE,paste("SSE.X2 = ",round(SSE.X1-SSE.X1pX2,3)),paste("SSE.X1 = ",round(SSE.X2-SSE.X1pX2,3))),paste("SSE.X1:X2 = ",round(SSE.X1pX2-SSE.X1iX2,3)),paste("SSE = ",round(SSE.X1iX2,3))),bty="n")
  
+legend("bottom",title="P-value",legend=c(paste("X1 = ",round(anovaII$"Pr(>F)"[1],6)),paste("X2 = ",round(anovaII$"Pr(>F)"[2],6)),paste("X1:X2 = ",round(anovaII$"Pr(>F)"[3],6))),bty="n",inset=.2) 
+
 #type III
 
 plot(seq(1,10),seq(1,10),type="n",xlab="",ylab="",xaxt="n",yaxt="n",bty="n")
- 
+rect(1,1,10,10,col="wheat4") 
 legend("top",inset=.2,title=ifelse(change.order==FALSE,"Type III Sum of Squares\n\nModel: Y = X1 + X2 + X1:X2","Type III Sum of Squares\n\nModel: Y = X2 + X1 + X1:X2"),legend=c(
  ifelse(change.order==FALSE,paste("SSE.X1 = ",round(SSE.noX1-SSE.X1iX2,3)),paste("SSE.X2 = ",round(SSE.noX2-SSE.X1iX2,3))), ifelse(change.order==FALSE,paste("SSE.X2 = ",round(SSE.noX2-SSE.X1iX2,3)),paste("SSE.X1 = ",round(SSE.noX1-SSE.X1iX2,3))),paste("SSE.X1:X2 = ",round(SSE.X1pX2-SSE.X1iX2,3)),paste("SSE = ",round(SSE.X1iX2,3))),bty="n")
+
+legend("bottom",title="P-value",legend=c(paste("X1 = ",round(anovaIII$"Pr(>F)"[2],6)),paste("X2 = ",round(anovaIII$"Pr(>F)"[3],6)),paste("X1:X2 = ",round(anovaIII$"Pr(>F)"[4],6))),bty="n",inset=.2)
+
  }
 
 
@@ -68,7 +83,7 @@ dialog.sd <- function() {
   r.order<-tclVar(0)
   done <- tclVar(0)
  
-  reset<-function(){
+reset<-function(){
 Y<-"c(16.5,18.4,12.7,14.0,12.8,14.5,11.0,10.8,14.3,10.0,39.1,26.2, 21.3,35.8,40.2,32.0,23.8,28.8,25.0,29.3)"
 X1<-"c(1,1,1,1,1,-1,-1,-1,-1,-1,1,1,1,1,1,-1,-1,-1,-1,-1)"
 X2<-"c(1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)"
